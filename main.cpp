87dbb1616e82702ec7eec22311d448b8d6ad4204
Vladimir Petrigo <taenaru@gmail.com>
2015-06-29 15:05:06 +0300

    Finish very first dirty example how does it work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Window.h"
 #include "GUI.h"
 #include "Analog_dial.hpp"
+#include "Clock.hpp"
+#include <unistd.h>
 
 using namespace std;
 using namespace Graph_lib;
@@ -12,17 +14,20 @@ const Point clock_xy {window_w / 2, window_h / 2};
 constexpr int dial_rad = 150;
 constexpr int arrow_rad = 100;
 
-
 int main() {
   Graph_lib::Window win {xy, window_w, window_h, "Test"};
-    
+
   Analog_Dial ac {clock_xy, dial_rad};
-  //  Clocks c{clock_xy, arrow_rad};
+  Clocks c{clock_xy, dial_rad};
   
   win.attach(ac);
-  //win.attach(c);
-  
-  gui_main();
-  
+  win.attach(c);
+
+  while (Fl::wait()) {
+    sleep(1);
+    c.update_clock();
+    Fl::redraw();
+  }
+
   return 0;
 }
