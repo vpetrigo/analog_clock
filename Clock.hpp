4c51453717fb4d837d06ac952eb9054ba687aaf5
Vladimir Petrigo <taenaru@gmail.com>
2015-06-29 15:04:12 +0300

    Add the clock arrows functionality

--- Clock.hpp
+++ Clock.hpp
@@ -1,94 +1,93 @@
 #ifndef CLOCK_HPP
 #define CLOCK_HPP
 
+#include <ctime>
 #include "Graph.h"
+#include "Angles.hpp"
+
+struct Time {
+    Time() {
+        time_refresh();
+    }
+
+    vector<int> get_time() const {
+        return vector<int> {hour, min, sec};
+    }
+
+    void update_time() {
+        time_refresh();
+    }
+
+ private:
+    void time_refresh() {
+        struct tm * timeinfo = nullptr;
+
+        time(&tm);
+        timeinfo = localtime(&tm);
+
+        hour = timeinfo->tm_hour;
+        min = timeinfo->tm_min;
+        sec = timeinfo->tm_sec;
+    }
+
+    int hour;
+    int min;
+    int sec;
+
+    time_t tm;
+};
 
 struct Clocks : Graph_lib::Lines {  
     Clocks(const Point& center, const int hm_rad) : c{center}, cl_rad{hm_rad} {
-        time_t tm;
-        struct tm * timeinfo = nullptr;
-        
-        time (&tm);
-        timeinfo = localtime(&tm);
-        
-        hour.set_val(timeinfo->tm_hour);
-        min.set_val(timeinfo->tm_min);
-        sec.set_val(timeinfo->tm_sec);
-        
-        add(c, conv_tm_to_coord(hour));
-        add(c, conv_tm_to_coord(min));
-        add(c, conv_tm_to_coord(sec));
+        vector<int> cur_time = t.get_time();
+
+        add(c, calc_hourarr_coords(cur_time[0]));
+        add(c, calc_minarr_coords(cur_time[1]));
+        add(c, calc_secarr_coords(cur_time[2]));
     }
-    
+
+    void update_clock() {
+        t.update_time();
+        vector<int> cur_time = t.get_time();
+
+        set_point(1, calc_hourarr_coords(cur_time[0]));
+        set_point(3, calc_minarr_coords(cur_time[1]));
+        set_point(5, calc_secarr_coords(cur_time[2]));
+    }
+
+    static constexpr int hours_on_dial = 12;
+    static constexpr int mins_on_dial = 60;
+    static constexpr int secs_on_dial = 60;
+
+    static constexpr int degs_b_hours = 360 / hours_on_dial;
+    static constexpr int degs_b_mins = 360 / mins_on_dial;
+    static constexpr int degs_b_secs = 360 / secs_on_dial;
+
+    static constexpr int start_dial_pos = 90;
+
  private:
-    struct Hour {
-        explicit Hour(int h) : h_{h} {}
-        
-        static constexpr int num_of_div = 12;
-        static constexpr int deg_p_dib = 360 / num_of_div;
-        
-        const int get_val() const {
-            return h_;
-        }
-        
-        void set_val(const int h) {
-            h_ = h;
-        }
-     private:
-        int h_;
-    };
-    
-    struct Minutes {
-        explicit Minutes(int m) : m_{m} {}
-        
-        static constexpr int num_of_div = 60;
-        static constexpr int deg_p_dib = 360 / num_of_div;
-        
-        const int get_val() const {
-            return m_;
-        }
-        
-        void set_val(const int m) {
-            m_ = m;
-        }
-     private:
-        int m_;
-    };
-    
-    struct Seconds {
-        explicit Seconds(int s) : s_{s} {}
-        
-        static constexpr int num_of_div = 60;
-        static constexpr int deg_p_dib = 360 / num_of_div;
-        
-        const int get_val() const {
-            return s_;
-        }
-        
-        void set_val(const int s) {
-            s_ = s;
-        }
-     private:
-        int s_;
-    };
- 
-    template <class T>
-    Point conv_tm_to_coord(const T& time) {
-        int x, y;  
-        double rad_andle = deg_to_rad(time.get_val() % T::num_of_div * T::deg_p_dib - 90);
-       
-        x = c.x + cl_rad * cos(rad_andle);
-        y = c.y + cl_rad * sin(rad_andle);
-        
-        return Point {x, y};
+    Point calc_hourarr_coords(int hour) const {
+        double angle = hour % hours_on_dial * degs_b_hours;
+
+        return circle_coords(c, cl_rad - 40, angle - start_dial_pos);
     }
-    
+
+    Point calc_minarr_coords(int min) const {
+        double angle = min * degs_b_mins;
+
+        return circle_coords(c, cl_rad - 25, angle - start_dial_pos);
+    }
+
+    Point calc_secarr_coords(int sec) const {
+        double angle = sec * degs_b_secs;
+
+        return circle_coords(c, cl_rad - 10, angle - start_dial_pos);
+    }
+
     Point c;
     int cl_rad;
-    
-    Hour hour{0};
-    Minutes min{0};
-    Seconds sec{0};
+
+    Time t;
 };
 
 #endif // CLOCK_HPP
